806c30f9d7b323b52167f4ead18f31a26f517f8c
pedroa098 <pedro12345augusto098@gmail.com>
2024-06-06 21:32:31 -0300

    Criada a navegaçâo enrtre as telelas

--- Comandas/FrmPrincipal.cs
+++ Comandas/FrmPrincipal.cs
@@ -6,5 +6,27 @@ namespace Comandas
         {
             InitializeComponent();
         }
+
+        // evento de click
+        private void btnCardapio_Click(object sender, EventArgs e)
+        {
+            // cria o formlario e exibe
+            new FrmCardapio().ShowDialog();
+        }
+
+        private void btnUsuarios_Click(object sender, EventArgs e)
+        {
+            new FrmUsuarios().ShowDialog();
+        }
+
+        private void btnComanda_Click(object sender, EventArgs e)
+        {
+            new FrmComanda().ShowDialog(); 
+        }
+
+        private void btnPedidoCozinha_Click(object sender, EventArgs e)
+        {
+            new FrmPedidoCozinha().ShowDialog();
+        }
     }
 }
