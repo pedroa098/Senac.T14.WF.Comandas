8ef56d98384ffc0a2a98b2bee914ad2675b01dab
pedroa098 <pedro12345augusto098@gmail.com>
2024-07-11 20:07:59 -0300

    Metodo atualizado ususario

--- Comandas/FrmUsuarios.Designer.cs
+++ Comandas/FrmUsuarios.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             btnVoltar = new ReaLTaiizor.Controls.CyberButton();
+            btnSalvar = new ReaLTaiizor.Controls.CyberButton();
+            btnNovo = new ReaLTaiizor.Controls.CyberButton();
+            btnExcluir = new ReaLTaiizor.Controls.CyberButton();
+            btnCancelar = new ReaLTaiizor.Controls.CyberButton();
             SuspendLayout();
             // 
             // btnVoltar
@@ -72,11 +76,177 @@
             btnVoltar.Timer_RGB = 300;
             btnVoltar.Click += btnVoltar_Click;
             // 
+            // btnSalvar
+            // 
+            btnSalvar.Alpha = 20;
+            btnSalvar.BackColor = Color.Transparent;
+            btnSalvar.Background = true;
+            btnSalvar.Background_WidthPen = 4F;
+            btnSalvar.BackgroundPen = true;
+            btnSalvar.ColorBackground = Color.FromArgb(37, 52, 68);
+            btnSalvar.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            btnSalvar.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            btnSalvar.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            btnSalvar.ColorLighting = Color.FromArgb(29, 200, 238);
+            btnSalvar.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            btnSalvar.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            btnSalvar.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            btnSalvar.Effect_1 = true;
+            btnSalvar.Effect_1_ColorBackground = Color.FromArgb(29, 200, 238);
+            btnSalvar.Effect_1_Transparency = 25;
+            btnSalvar.Effect_2 = true;
+            btnSalvar.Effect_2_ColorBackground = Color.White;
+            btnSalvar.Effect_2_Transparency = 20;
+            btnSalvar.Font = new Font("Arial", 11F);
+            btnSalvar.ForeColor = Color.FromArgb(245, 245, 245);
+            btnSalvar.Lighting = false;
+            btnSalvar.LinearGradient_Background = false;
+            btnSalvar.LinearGradientPen = false;
+            btnSalvar.Location = new Point(61, 76);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.PenWidth = 15;
+            btnSalvar.Rounding = true;
+            btnSalvar.RoundingInt = 70;
+            btnSalvar.Size = new Size(153, 41);
+            btnSalvar.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            btnSalvar.TabIndex = 1;
+            btnSalvar.Tag = "Cyber";
+            btnSalvar.TextButton = "Salvar";
+            btnSalvar.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            btnSalvar.Timer_Effect_1 = 5;
+            btnSalvar.Timer_RGB = 300;
+            // 
+            // btnNovo
+            // 
+            btnNovo.Alpha = 20;
+            btnNovo.BackColor = Color.Transparent;
+            btnNovo.Background = true;
+            btnNovo.Background_WidthPen = 4F;
+            btnNovo.BackgroundPen = true;
+            btnNovo.ColorBackground = Color.FromArgb(37, 52, 68);
+            btnNovo.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            btnNovo.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            btnNovo.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            btnNovo.ColorLighting = Color.FromArgb(29, 200, 238);
+            btnNovo.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            btnNovo.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            btnNovo.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            btnNovo.Effect_1 = true;
+            btnNovo.Effect_1_ColorBackground = Color.FromArgb(29, 200, 238);
+            btnNovo.Effect_1_Transparency = 25;
+            btnNovo.Effect_2 = true;
+            btnNovo.Effect_2_ColorBackground = Color.White;
+            btnNovo.Effect_2_Transparency = 20;
+            btnNovo.Font = new Font("Arial", 11F);
+            btnNovo.ForeColor = Color.FromArgb(245, 245, 245);
+            btnNovo.Lighting = false;
+            btnNovo.LinearGradient_Background = false;
+            btnNovo.LinearGradientPen = false;
+            btnNovo.Location = new Point(220, 76);
+            btnNovo.Name = "btnNovo";
+            btnNovo.PenWidth = 15;
+            btnNovo.Rounding = true;
+            btnNovo.RoundingInt = 70;
+            btnNovo.Size = new Size(153, 41);
+            btnNovo.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            btnNovo.TabIndex = 2;
+            btnNovo.Tag = "Cyber";
+            btnNovo.TextButton = "Novo";
+            btnNovo.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            btnNovo.Timer_Effect_1 = 5;
+            btnNovo.Timer_RGB = 300;
+            btnNovo.Click += btnNovo_Click;
+            // 
+            // btnExcluir
+            // 
+            btnExcluir.Alpha = 20;
+            btnExcluir.BackColor = Color.Transparent;
+            btnExcluir.Background = true;
+            btnExcluir.Background_WidthPen = 4F;
+            btnExcluir.BackgroundPen = true;
+            btnExcluir.ColorBackground = Color.FromArgb(37, 52, 68);
+            btnExcluir.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            btnExcluir.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            btnExcluir.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            btnExcluir.ColorLighting = Color.FromArgb(29, 200, 238);
+            btnExcluir.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            btnExcluir.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            btnExcluir.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            btnExcluir.Effect_1 = true;
+            btnExcluir.Effect_1_ColorBackground = Color.FromArgb(29, 200, 238);
+            btnExcluir.Effect_1_Transparency = 25;
+            btnExcluir.Effect_2 = true;
+            btnExcluir.Effect_2_ColorBackground = Color.White;
+            btnExcluir.Effect_2_Transparency = 20;
+            btnExcluir.Font = new Font("Arial", 11F);
+            btnExcluir.ForeColor = Color.FromArgb(245, 245, 245);
+            btnExcluir.Lighting = false;
+            btnExcluir.LinearGradient_Background = false;
+            btnExcluir.LinearGradientPen = false;
+            btnExcluir.Location = new Point(379, 76);
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.PenWidth = 15;
+            btnExcluir.Rounding = true;
+            btnExcluir.RoundingInt = 70;
+            btnExcluir.Size = new Size(153, 41);
+            btnExcluir.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            btnExcluir.TabIndex = 3;
+            btnExcluir.Tag = "Cyber";
+            btnExcluir.TextButton = "Excluir";
+            btnExcluir.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            btnExcluir.Timer_Effect_1 = 5;
+            btnExcluir.Timer_RGB = 300;
+            btnExcluir.Click += btnExcluir_Click;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Alpha = 20;
+            btnCancelar.BackColor = Color.Transparent;
+            btnCancelar.Background = true;
+            btnCancelar.Background_WidthPen = 4F;
+            btnCancelar.BackgroundPen = true;
+            btnCancelar.ColorBackground = Color.FromArgb(37, 52, 68);
+            btnCancelar.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            btnCancelar.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            btnCancelar.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            btnCancelar.ColorLighting = Color.FromArgb(29, 200, 238);
+            btnCancelar.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            btnCancelar.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            btnCancelar.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            btnCancelar.Effect_1 = true;
+            btnCancelar.Effect_1_ColorBackground = Color.FromArgb(29, 200, 238);
+            btnCancelar.Effect_1_Transparency = 25;
+            btnCancelar.Effect_2 = true;
+            btnCancelar.Effect_2_ColorBackground = Color.White;
+            btnCancelar.Effect_2_Transparency = 20;
+            btnCancelar.Font = new Font("Arial", 11F);
+            btnCancelar.ForeColor = Color.FromArgb(245, 245, 245);
+            btnCancelar.Lighting = false;
+            btnCancelar.LinearGradient_Background = false;
+            btnCancelar.LinearGradientPen = false;
+            btnCancelar.Location = new Point(538, 76);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.PenWidth = 15;
+            btnCancelar.Rounding = true;
+            btnCancelar.RoundingInt = 70;
+            btnCancelar.Size = new Size(153, 41);
+            btnCancelar.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            btnCancelar.TabIndex = 4;
+            btnCancelar.Tag = "Cyber";
+            btnCancelar.TextButton = "Cancelar";
+            btnCancelar.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            btnCancelar.Timer_Effect_1 = 5;
+            btnCancelar.Timer_RGB = 300;
+            // 
             // FrmUsuarios
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnExcluir);
+            Controls.Add(btnNovo);
+            Controls.Add(btnSalvar);
             Controls.Add(btnVoltar);
             FormBorderStyle = FormBorderStyle.None;
             Name = "FrmUsuarios";
@@ -88,5 +258,9 @@
         #endregion
 
         private ReaLTaiizor.Controls.CyberButton btnVoltar;
+        private ReaLTaiizor.Controls.CyberButton btnSalvar;
+        private ReaLTaiizor.Controls.CyberButton btnNovo;
+        private ReaLTaiizor.Controls.CyberButton btnExcluir;
+        private ReaLTaiizor.Controls.CyberButton btnCancelar;
     }
 }
--- Comandas/FrmUsuarios.cs
+++ Comandas/FrmUsuarios.cs
@@ -12,6 +12,8 @@ namespace Comandas
 {
     public partial class FrmUsuarios : Form
     {
+        private bool ehNovo;
+
         public FrmUsuarios()
         {
             InitializeComponent();
@@ -20,6 +22,65 @@ namespace Comandas
         private void btnVoltar_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (ehNovo)
+                //metodo que ira inserir usuario no banco
+                CriarUsuario();
+
+            else
+                //AtualizarUsuario();
+                AtualizarUsuario();
+        }
+
+        private void AtualizarUsuario()
+        {
+            using (var banco = new AppDbContext())
+            {
+                var usuario = banco
+                    .Usuarios
+                    .Where(e => e.Id == 1)
+                    .FirstOrDefault();
+                usuario.Nome = "Vine";
+                usuario.Email = "pedro@pedro.com";
+                banco.SaveChanges();
+                throw new NotImplementedException();
+            }
+        }
+
+        private void CriarUsuario()
+        {
+            //acessar o banco 
+
+            using (var banco = new AppDbContext())
+            {
+                //criar variavel
+                var novoUsuario = new Usuario();
+                novoUsuario.Nome = "Pedro";
+                novoUsuario.Email = "pedro12345augusto098@gmail.com";
+                novoUsuario.Senha = "123";
+                //salvar as alterãções(INSERT INTO usuarios)
+                banco.Usuarios.Add(novoUsuario);
+                banco.SaveChanges();
+
+                MessageBox.Show("Usuário cadastrado com sucesso");
+            }
+
+            //criar novo ususario 
+
+            //salvar alterações
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            ehNovo = true;
         }
     }
 }
